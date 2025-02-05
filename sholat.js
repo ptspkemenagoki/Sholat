c83a59b80f6bc91e4de8833ca8b2dbf91e9a6177
ptspkemenagoki <81477903+ptspkemenagoki@users.noreply.github.com>
2025-02-06 00:53:01 +0700

    Update sholat.js

--- sholat.js
+++ sholat.js
@@ -26,18 +26,17 @@ const tanggal = `${getYear}-${bulan()}-${hari()}`;
 
 
 function getJadwalSholat() {
-	fetch('https://api.myquran.com/v2/sholat/jadwal/1632/2024-06-23)
-	.then(response => response.json())
-	.then(data => {
-		const jadwal = data.jadwal.data;
-		document.querySelector('.subuh').textContent = jadwal.subuh;
-		document.querySelector('.dzuhur').textContent = jadwal.dzuhur;
-		document.querySelector('.ashar').textContent = jadwal.ashar;
-		document.querySelector('.maghrib').textContent = jadwal.maghrib;
-		document.querySelector('.isya').textContent = jadwal.isya;
-		document.querySelector('.tanggal').textContent = jadwal.tanggal;
-		
-	});
+	fetch('https://api.myquran.com/v2/sholat/jadwal/0809/2024-06-23')
+	.then(res => res.json())
+	.then ( data => {
+		const jadwal = data.data.jadwal
+		// console.log(jadwal)
+		document.getElementById('subuh').textContent = jadwal.subuh
+		document.getElementById('dzuhur').textContent = jadwal.dzuhur
+		document.getElementById('ashar').textContent = jadwal.ashar
+		document.getElementById('maghrib').textContent = jadwal.maghrib
+		document.getElementById('isya').textContent = jadwal.isya
+	})
 }
 
-getJadwalSholat();
+getJadwalSholat()
